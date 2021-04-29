ad96a8434450604536193699f894d697cbc89e4f
lewis <kemboilewis@gmail.com>
2021-04-29 16:00:48 +0300

    included user_account.urls

--- src/rentalink_project/urls.py
+++ src/rentalink_project/urls.py
@@ -1,7 +1,10 @@
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns = [
+urlpatterns= [
     path('admin/', admin.site.urls),
-]
+    path('user_account/', include('user_account.urls')),
+
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
