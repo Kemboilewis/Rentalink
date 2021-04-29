295104b31a8d6b1613b96ad9a73cea01ef1d8772
lewis <kemboilewis@gmail.com>
2021-04-29 15:23:15 +0300

    created model  myUser

--- src/user_account/models.py
+++ src/user_account/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class myUser(AbstractUser ):
+    is_tenant = models.BooleanField('tenant status', default=False)
+    is_landlord = models.BooleanField('landlord status', default=False)
+    id_number = models.CharField(max_length=20, default="not provided")
+    Phone_number = models.CharField(max_length=10, default="1234567890")
+    #birth_date = models.DateField(default=2000/01/10)
+    profile_image=models.ImageField(upload_to='images/',default='image')
+    id_copy = models.FileField(upload_to='documents/', default='document')
+    
